5cb9de4e7e8be03cd7ff5a0d9316a6da22a8766a
SinovuyoMathontsi <sinovuyomathontsi@gmail.com>
2025-04-07 00:31:15 +0200

    Add CRUD functionality for courses

---
+++ SpringAssignment1/src/main/java/com/ufh/SpringAssignment1/model/Course.java
@@ -0,0 +1,41 @@
+package com.ufh.SpringAssignment1.model;
+
+public class Course {
+    private Long id;
+    private String name;
+    private String type; // e.g., "Foundation", "Undergraduate", "Honours"
+
+    // Constructors
+    public Course() {}
+
+    public Course(Long id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
